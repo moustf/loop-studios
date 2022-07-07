ce6c41a9d7b98a1962726795401dc361ba746ad3
moustf <tepro.18@gmail.com>
2022-07-07 18:35:49 +0300

    Formatted the code with 4 spaces tabs

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function App() {
-  return (
-    <>
-      <h1>Mustafa Salem</h1>
-      <h4>Junior Web Developer</h4>
-    </>
-  );
+export default function App () {
+    return (
+        <>
+            <h1>Mustafa Salem</h1>
+            <h4>Junior Web Developer</h4>
+            <p>Hi poplllllleeee</p>
+        </>
+    );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,51 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlPlugin = new HtmlWebpackPlugin({
-  template: "src/index.html",
-  filename: "index.html",
+    template: "src/index.html",
+    filename: "index.html"
 });
 
 module.exports = {
-  mode: "development",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: "url-loader",
-        options: { limit: false },
-      },
-    ],
-  },
-  plugins: [htmlPlugin],
-  devServer: {
-    hot: true,
-    liveReload: true,
-    open: true,
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts"],
-  },
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: "url-loader",
+                options: { limit: false }
+            },
+            {
+                test: /\.(js|jsx)/,
+                exclude: /node_modules/,
+                use: ["babel-loader", "eslint-loader"]
+            }
+        ]
+    },
+    plugins: [htmlPlugin],
+    devServer: {
+        compress: true,
+        hot: true,
+        liveReload: true,
+        open: true
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json", ".ts"]
+    }
 };
