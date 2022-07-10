b7f245dfa5046581b64a93efa755b545dc907884
moustf <tepro.18@gmail.com>
2022-07-10 13:58:00 +0300

    Added the clicks functions and events and edited some texts close #5

--- src/App/LandingPage/Header/Header.js
+++ src/App/LandingPage/Header/Header.js
@@ -6,11 +6,22 @@ import SVG from "react-inlinesvg";
 import "./Header.scss";
 
 export default function Header () {
+    const [isMenu, setIsMenu] = React.useState(false);
+    const handleClick = () => {
+        setIsMenu((prevIsMenu) => !prevIsMenu);
+    };
+    const menuStyle = {
+        display: isMenu ? "flex" : "none"
+    };
+    const handleClose = () => {
+        setIsMenu((prevIsMenu) => !prevIsMenu);
+    };
+
     return (
         <header className="header">
             <SVG className="header__logo" src={Logo} alt="logo" />
-            <FaBars className="header__bars" />
-            <div className="header__nav-mobile">
+            <FaBars className="header__bars" onClick={handleClick} />
+            <div className="header__nav-mobile" style={menuStyle}>
                 <div className="images">
                     <SVG
                         className="header__nav-mobile__logo"
@@ -21,6 +32,7 @@ export default function Header () {
                         className="header__nav-mobile__close"
                         src={Close}
                         alt="close"
+                        onClick={handleClose}
                     />
                 </div>
                 <div className="tags">
@@ -40,23 +52,23 @@ export default function Header () {
                         SUPPORT
                     </a>
                 </div>
-                <div className="header__nav-desktop">
-                    <a className="about" href="#about">
-                        ABOUT
-                    </a>
-                    <a className="careers" href="#careers">
-                        CAREERS
-                    </a>
-                    <a className="events" href="#events">
-                        EVENTS
-                    </a>
-                    <a className="products" href="#products">
-                        PRODUCTS
-                    </a>
-                    <a className="support" href="#support">
-                        SUPPORT
-                    </a>
-                </div>
+            </div>
+            <div className="header__nav-desktop">
+                <a className="about" href="#about">
+                    About
+                </a>
+                <a className="careers" href="#careers">
+                    Careers
+                </a>
+                <a className="events" href="#events">
+                    Events
+                </a>
+                <a className="products" href="#products">
+                    Products
+                </a>
+                <a className="support" href="#support">
+                    Support
+                </a>
             </div>
         </header>
     );
