b1d7b059e21a78bf571b8088550d6e2c32567ded
moustf <tepro.18@gmail.com>
2022-07-10 13:59:19 +0300

    Added the images as an elements, and added an overlay div close #5

--- src/App/LandingPage/LandingPage.js
+++ src/App/LandingPage/LandingPage.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "./Header/Header";
+import HeroMobile from "../../../assets/mobile/image-hero.jpg";
+import HeroDesktop from "../../../assets/desktop/image-hero.jpg";
 import "./LandingPage.scss";
 
 export default function LandingPage () {
     return (
         <section className="landing-page">
+            <img className="hero" src={HeroMobile} alt="hero mobile" />
+            <img className="hero-desktop" src={HeroDesktop} alt="hero desktop" />
+            <div className="overlay" />
             <Header />
-            <p calssName="landing-page__para">
+            <p className="landing-page__para">
                 IMMERSIVE EXPERIENCES THAT DELIVER
             </p>
         </section>
