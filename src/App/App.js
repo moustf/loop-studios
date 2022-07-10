c57ab236ca767bc097fd80dccfcea06b5268160d
moustf <tepro.18@gmail.com>
2022-07-10 19:30:56 +0300

    Added the Creators component close #6

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import LandingPage from "./LandingPage/LandingPage";
+import Creators from "./Creators/Creators";
 import "./App.scss";
 
 export default function App () {
     return (
         <div className="container">
             <LandingPage />
+            <Creators />
         </div>
     );
 }
