1efd8b45fea42dbc26472e9c3ca7a3f846c79cf6
moustf <tepro.18@gmail.com>
2022-07-11 22:27:35 +0300

    Added the OurCreations component and the Footer component close #7 close #8

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import LandingPage from "./LandingPage/LandingPage";
 import Creators from "./Creators/Creators";
+import OurCreations from "./OurCreations/OurCreations";
+import Footer from "./Footer/Footer";
 import "./App.scss";
 
 export default function App () {
@@ -8,6 +10,8 @@ export default function App () {
         <div className="container">
             <LandingPage />
             <Creators />
+            <OurCreations />
+            <Footer />
         </div>
     );
 }
